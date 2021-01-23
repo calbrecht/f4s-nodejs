9c261240c78bb510b2b92d7b9ed991874f6a86c8
Christian Albrecht <christian.albrecht@mayflower.de>
2021-01-23 11:54:13 +0100

    Recreate lock file after updating & pushing fresh node-packages

--- .github/flake.nix
+++ .github/flake.nix
@@ -26,10 +26,12 @@
           ${git_bin} config user.name "$user_name"
           ${git_bin} config user.email "$user_mail"
 
-          ${git_bin} add . >&2
+          ${git_bin} add pkgs >&2
 
           ${git_bin} commit -m "Update node-packages" >&2
+          ${git_bin} push >&2
 
+          ${nix_bin} flake update --recreate-lock-file --commit-lock-file
           ${git_bin} push >&2
         '';
 
