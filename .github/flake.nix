9cc1598fb0873d48537bbb1fd06b287ee1f6507c
Christian Albrecht <christian.albrecht@mayflower.de>
2026-04-26 15:52:29 +0200

    Update nodejs-tools

--- .github/flake.nix
+++ .github/flake.nix
@@ -2,15 +2,16 @@
   description = "Update node-packages.";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/master;
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${system}";
 
-      nix_bin = pkgs.nixFlakes + /bin/nix;
+      nix_bin = pkgs.nix + /bin/nix;
       git_bin = pkgs.git + /bin/git;
     in
     {
@@ -22,7 +23,7 @@
           user_name=''${git_user_name:-$(${git_bin} config user.name)}
           user_mail=''${git_user_mail:-$(${git_bin} config user.email)}
 
-          test -z "$(${git_bin} status pkgs -s)" && {
+          test -z "$(${git_bin} status . -s)" && {
             : Nothing new, exiting.
             exit 0
           }
@@ -30,21 +31,31 @@
           ${git_bin} config user.name "$user_name"
           ${git_bin} config user.email "$user_mail"
 
-          ${git_bin} add pkgs >&2
+          ${git_bin} add . >&2
 
-          ${git_bin} commit -m "Update node-packages" >&2
+          ${git_bin} commit -m "Update nodejs-tools" >&2
           ${git_bin} push >&2
 
           ${nix_bin} flake update --commit-lock-file
           ${git_bin} push >&2
         '';
 
+        hash-update = pkgs.writeScriptBin "hash-update" ''
+          #!${pkgs.stdenv.shell}
+          set -xeu
+
+          sed -i 's/nodejsToolsHash = [^;]\+;/nodejsToolsHash = lib.fakeHash;/' flake.nix
+          sha=$(nix build .#nodejs-tools 2>&1 | grep got: | grep -oE 'sha.*')
+          sed -i 's|nodejsToolsHash = [^;]\+;|nodejsToolsHash = "'"''${sha}"'";|' flake.nix
+        '';
+
         auto-update = pkgs.writeScriptBin "auto-update" ''
           #!${pkgs.stdenv.shell}
           set -xeu
 
-          ${nix_bin} run ./#node2nixup && \
-          . ${self.packages."${system}".commit-and-push}/bin/commit-and-push || \
+          ${nix_bin} run ./#pnpm -- up --latest && \
+          . ${self.packages."${system}".hash-update}/bin/hash-update && true #\
+          #. ${self.packages."${system}".commit-and-push}/bin/commit-and-push || \
           true
         '';
       };
--- flake.nix
+++ flake.nix
@@ -2,239 +2,104 @@
   description = "nodejs tooling.";
 
   inputs = {
-    pkgs-src = { url = "github:calbrecht/f4s-nodejs?dir=pkgs"; flake = false; };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, pkgs-src }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      };
-      pkgs-dir = pkgs-src; #+ /pkgs;
-      node-packages = [
-        "eslint"
-        "eslint_d"
-        #"import-js"
-        #"jison" # devDependency of jsonlint
-        "jsonlint"
-        "node-gyp"
-        "node-gyp-build"
-        "node-pre-gyp"
-        "prettier"
-        "standardx"
-        "testcafe-browser-tools"
-        "trepan-ni"
-        "tslint"
-        "typescript"
-        "typescript-language-server"
-        "bash-language-server"
-        "intelephense"
-        "yaml-language-server"
-      ];
-      nodejs_latest = pkgs.nodejs_latest;
-    in
-    {
-      apps."${system}".node2nixup = {
-        type = "app";
-        program = (pkgs.writeScriptBin "node2nix-update-node-packages.sh" ''
-          set -e
-          cd ./pkgs
-          rm ./node-env.nix
-          echo '${builtins.toJSON node-packages}' > node-packages.json
-          ${pkgs.nodePackages.node2nix}/bin/node2nix \
-            -i node-packages.json -o node-packages.nix -c composition.nix
-        '') + /bin/node2nix-update-node-packages.sh;
-      };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        withSystem,
+        lib,
+        ...
+      }:
+      let
+        x86_64 = "x86_64-linux";
+        nodejsToolsHash = "sha256-1Ovf59U/JssT67i0wprFXAU236juMyFbqvG+Fl141fM=";
+      in
+      {
+        systems = [ x86_64 ];
+        flake = withSystem x86_64 (
+          { ... }:
+          {
+            overlays = {
+              default = (
+                final: prev: {
+                  pnpm = prev.pnpm_10.override { nodejs = prev.nodejs_latest; };
 
-      legacyPackages."${system}" = {
-        inherit (pkgs) nodejs nodePackages;
-        inherit nodejs_latest;
-        nodePackages_latest = pkgs.lib.dontRecurseIntoAttrs nodejs_latest.pkgs;
-      };
+                  nodejs-tools = prev.stdenv.mkDerivation (finalAttrs: {
+                    pname = "nodejs-tools";
+                    version = "latest";
 
-      defaultPackage."${system}" = self.legacyPackages."${system}".nodejs;
+                    src = ./.;
 
-      overlay = final: prev:
-        let
-          _nodePackages = (prev.callPackage pkgs-dir { pkgs = final; });
-          _nodePackages_latest = (prev.callPackage pkgs-dir { pkgs = final; nodejs = final.nodejs_latest; });
-        in
-        {
-          #nodejs_legacy = prev.nodejs;
-          #nodejs = prev.nodejs_latest;
-          #
-          utillinux = prev.util-linux;
-          nodePackages = prev.nodePackages // _nodePackages // {
-            eslint_d = (_nodePackages.eslint_d.override {
-              dontNpmInstall = true;
-              preRebuild = ''
-                substituteInPlace bin/eslint_d.js --replace "'../lib/options'" "'../lib/options-cliengine'"
-                modules=$out/lib/node_modules
-                mkdir -p $modules/.bin
-                ln -s $modules/eslint_d/bin/eslint_d.js $modules/.bin/eslint_d
-                ln -s $modules/eslint_d/bin/eslint.js $modules/.bin/eslint
-              '';
-            });
+                    nativeBuildInputs = [
+                      prev.nodejs_latest
+                      prev.pnpmConfigHook
+                      final.pnpm
+                    ];
+
+                    pnpmDeps = prev.fetchPnpmDeps {
+                      inherit (finalAttrs) pname version src;
+                      pnpm = final.pnpm;
+                      fetcherVersion = 3;
+                      hash = nodejsToolsHash;
+                    };
+
+                    installPhase = ''
+                      mkdir -p $out
+                      cp -r ./node_modules/.pnpm $out/.pnpm
+                      cp -r ./node_modules/.bin $out/bin
+                    '';
+                  });
+                }
+              );
+              #eslint_d = (_nodePackages.eslint_d.override {
+              #  dontNpmInstall = true;
+              #  preRebuild = ''
+              #    substituteInPlace bin/eslint_d.js --replace "'../lib/options'" "'../lib/options-cliengine'"
+              #  modules=$out/lib/node_modules
+              #  mkdir -p $modules/.bin
+              #  ln -s $modules/eslint_d/bin/eslint_d.js $modules/.bin/eslint_d
+              #  ln -s $modules/eslint_d/bin/eslint.js $modules/.bin/eslint
+              #  '';
+              #});
+            };
+          }
+        );
+        perSystem =
+          {
+            system,
+            pkgs,
+            ...
+          }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+              overlays = [
+                top.config.flake.overlays.default
+              ];
+            };
+
+            legacyPackages = {
+              inherit (pkgs)
+                nodejs
+                nodejs_latest
+                pnpm
+                nodejs-tools
+                ;
+            };
+
+            formatter =
+              (inputs.treefmt-nix.lib.evalModule pkgs {
+                programs.nixfmt.enable = true;
+              }).config.build.wrapper;
           };
-
-          #nodejs_latest = pkgs.nodejs-18_x;
-
-          nodePackages_latest = prev.nodePackages_latest
-          // (pkgs.lib.dontRecurseIntoAttrs final.nodejs_latest.pkgs)
-          // _nodePackages_latest
-          // {
-            # npm tries to fetch dev dependencies nowadays despite --production is given
-            # https://github.com/npm/cli/issues/1969
-            #node2nix = (prev.nodePackages_latest.node2nix.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/node2nix/bin/node2nix.js $modules/.bin/node2nix
-            #  '';
-            #});
-            #eslint = (_nodePackages_latest.eslint.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/eslint/bin/eslint.js $modules/.bin/eslint
-            #  '';
-            #});
-            #eslint_d = (_nodePackages_latest.eslint_d.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    substituteInPlace bin/eslint_d.js --replace "'../lib/options'" "'../lib/options-cliengine'"
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/eslint_d/bin/eslint_d.js $modules/.bin/eslint_d
-            #    ln -s $modules/eslint_d/bin/eslint.js $modules/.bin/eslint
-            #  '';
-            #});
-            #jsonlint = (_nodePackages_latest.jsonlint.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/jsonlint/lib/cli.js $modules/.bin/jsonlint
-            #  '';
-            #});
-            #node-pre-gyp = (_nodePackages_latest.node-pre-gyp.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/node-pre-gyp/bin/node-pre-gyp $modules/.bin/node-pre-gyp
-            #  '';
-            #});
-            #node-gyp = (_nodePackages_latest.node-gyp.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/node-gyp/bin/node-gyp $modules/.bin/node-gyp
-            #  '';
-            #});
-            #node-gyp-build = (_nodePackages_latest.node-gyp-build.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/node-gyp-build/bin/node-gyp-build $modules/.bin/node-gyp-build
-            #  '';
-            #});
-            #import-js = (_nodePackages_latest.import-js.override {
-            #  dontNpmInstall = true;
-            #  buildInputs = [ final.nodePackages_latest.node-pre-gyp ];
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/import-js/bin/importjs.js $modules/.bin/importjs
-            #  '';
-            #});
-            #standardx = (_nodePackages_latest.standardx.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/standardx/bin/cmd.js $modules/.bin/standardx
-            #  '';
-            #});
-            #tslint = (_nodePackages_latest.tslint.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/tslint/bin/tslint $modules/.bin/tslint
-            #  '';
-            #});
-            #typescript = (_nodePackages_latest.typescript.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/typescript/bin/tsc $modules/.bin/tsc
-            #    ln -s $modules/typescript/bin/tsserver $modules/.bin/tsserver
-            #  '';
-            #});
-            #typescript-language-server = (_nodePackages_latest.typescript-language-server.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/typescript-language-server/lib/cli.js $modules/.bin/typescript-language-server
-            #    chmod 755 $modules/.bin/typescript-language-server
-            #  '';
-            #});
-            #testcafe-browser-tools = (_nodePackages_latest.testcafe-browser-tools.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/testcafe-browser-tools/bin/ $modules/.bin/
-            #  '';
-            #});
-            #trepan-ni = (_nodePackages_latest.trepan-ni.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/trepan-ni/cli.js $modules/.bin/trepan-ni
-            #    ln -s $modules/trepan-ni/cli.js $modules/.bin/cli.js
-            #  '';
-            #});
-            #bash-language-server = (_nodePackages_latest.bash-language-server.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/bash-language-server/bin/main.js $modules/.bin/bash-language-server
-            #  '';
-            #});
-            #intelephense = (_nodePackages_latest.intelephense.override {
-            #  dontNpmInstall = true;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/intelephense/lib/intelephense.js $modules/.bin/intelephense
-            #    ln -s $modules/intelephense/lib/intelephense.js $modules/.bin/intelephense.js
-            #    chmod 755 $modules/.bin/intelephense
-            #  '';
-            #});
-            #yaml-language-server = (_nodePackages_latest.yaml-language-server.override (oldAttrs: {
-            #  dontNpmInstall = true;
-            #  dependencies = [
-            #    _nodePackages_latest.prettier
-            #  ] ++ oldAttrs.dependencies;
-            #  preRebuild = ''
-            #    modules=$out/lib/node_modules
-            #    mkdir -p $modules/.bin
-            #    ln -s $modules/yaml-language-server/bin/yaml-language-server $modules/.bin/yaml-language-server
-            #  '';
-            #}));
-          };
-        };
-    };
+      }
+    );
 }
